d9069060f267568debe3c1e7d6a128b7356e689d
benapart <164779300+benapart@users.noreply.github.com>
2024-03-24 20:08:44 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 basic.forever(function () {
-    music.play(music.createSoundExpression(WaveShape.Sine, 500, 500, 255, 0, 50, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(WaveShape.Square, 1600, 1, 255, 0, 2000, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(WaveShape.Sawtooth, 500, 500, 255, 0, 5000, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(WaveShape.Sawtooth, 500, 500, 255, 0, 50, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(WaveShape.Sawtooth, 500, 500, 255, 0, 50, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     music.play(music.stringPlayable("C5 B - C5 B - A G ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 A G F - - - - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("G F C E - - - - ", 100), music.PlaybackMode.UntilDone)
 })
